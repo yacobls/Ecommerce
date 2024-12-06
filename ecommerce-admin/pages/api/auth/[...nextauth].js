7db0bbb3677f84d9a0fa12efbb758d39bde4d4c2
yacobls <yacob.lamrodo@gmail.com>
2024-12-06 09:50:08 +0700

    Add connection mongoDB

--- ecommerce-admin/pages/api/auth/[...nextauth].js
+++ ecommerce-admin/pages/api/auth/[...nextauth].js
@@ -1,12 +1,15 @@
-import NextAuth from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
-  ]
-})
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+});
