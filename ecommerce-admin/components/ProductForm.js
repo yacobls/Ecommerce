6bbb2db4dac202c6e0377b03acf45d89c8147f44
yacobls <yacob.lamrodo@gmail.com>
2024-12-11 12:15:39 +0700

    Add Category in Edit Product

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Spinner from "@/components/Spinner";
@@ -10,17 +10,25 @@ export default function ProductForm({
   description: existingDescription,
   price: existingPrice,
   images: existingImages,
+  category: assignedCategory,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
+  const [category, setCategory] = useState(assignedCategory || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState([]);
   const router = useRouter();
+  useEffect(() => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }, []);
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = { title, description, price, images };
+    const data = { title, description, price, images, category };
     if (_id) {
       //Update
       await axios.put("/api/products", { ...data, _id });
@@ -58,14 +66,21 @@ export default function ProductForm({
         type="text"
         placeholder="product name"
         value={title}
-        onChange={ev => setTitle(ev.target.value)}
+        onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Category</label>
+      <select value={category} onChange={(ev) => setCategory(ev.target.value)}>
+        <option value="">Uncategorized</option>
+        {categories.length > 0 &&
+          categories.map((c) => <option value={c._id}>{c.name}</option>)}
+      </select>
       <label>Photos</label>
       <div className="mb-2 flex flex-wrap gap-1">
         <ReactSortable
-        list={images}
-        className="flex flex-wrap gap-1"
-        setList={updateImagesOrder}>
+          list={images}
+          className="flex flex-wrap gap-1"
+          setList={updateImagesOrder}
+        >
           {!!images?.length &&
             images.map((link) => (
               <div key={link} className="h-24">
@@ -101,14 +116,14 @@ export default function ProductForm({
       <textarea
         placeholder="description"
         value={description}
-        onChange={ev => setDescription(ev.target.value)}
+        onChange={(ev) => setDescription(ev.target.value)}
       />
       <label>Price (in IDR)</label>
       <input
         type="number"
         placeholder="price"
         value={price}
-        onChange={ev => setPrice(ev.target.value)}
+        onChange={(ev) => setPrice(ev.target.value)}
       />
       <button type="submit" className="btn-primary">
         Save
