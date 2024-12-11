78e3f6bc92ddbd49d40693a5947dfe389f488553
yacobls <yacob.lamrodo@gmail.com>
2024-12-11 11:19:11 +0700

    Add Edit and Delete, and finish navbar Categories

--- ecommerce-admin/pages/categories.js
+++ ecommerce-admin/pages/categories.js
@@ -1,8 +1,10 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-export default function Categories() {
+function Categories({ swal }) {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
   const [parentCategory, setParentCategory] = useState("");
   const [categories, setCategories] = useState([]);
@@ -10,36 +12,71 @@ export default function Categories() {
     fetchCategories();
   }, []);
   function fetchCategories() {
-    axios.get("/api/categories").then(result => {
+    axios.get("/api/categories").then((result) => {
       setCategories(result.data);
     });
   }
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post("/api/categories", { name, parentCategory });
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
     setName("");
     fetchCategories();
+  }
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+  function deleteCategory(category) {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete ${category.name}?`,
+        showCancelButton: true,
+        cancelButtonText: "No",
+        confirmButtonText: "Yes",
+        confirmButtonColor: "#E30D0D",
+        reverseButtons: true,
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          const { _id } = category;
+          await axios.delete("/api/categories?_id=" + _id);
+          fetchCategories();
+        }
+      });
   }
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New Category Name</label>
+      <label>
+        {editedCategory
+          ? `Edit Category ${editedCategory.name}`
+          : "Create new Category"}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
           className="mb-0"
           type="text"
           placeholder={"category name"}
-          onChange={ev => setName(ev.target.value)}
+          onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
         <select
           className="mb-0"
-          onChange={ev => setParentCategory(ev.target.value)}
+          onChange={(ev) => setParentCategory(ev.target.value)}
           value={parentCategory}
         >
           <option value="">No parent category</option>
           {categories.length > 0 &&
-            categories.map(category => (
+            categories.map((category) => (
               <option value={category._id}>{category.name}</option>
             ))}
         </select>
@@ -52,14 +89,29 @@ export default function Categories() {
           <tr>
             <td>Category Name</td>
             <td>Parent Category</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
           {categories.length > 0 &&
-            categories.map(category => (
+            categories.map((category) => (
               <tr>
                 <td>{category.name}</td>
                 <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className="btn-primary mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className="btn-primary"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
@@ -67,3 +119,5 @@ export default function Categories() {
     </Layout>
   );
 }
+
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
--- ecommerce-admin/pages/products.js
+++ ecommerce-admin/pages/products.js
@@ -26,7 +26,7 @@ export default function Products() {
           </tr>
         </thead>
         <tbody>
-          {products.map(product => (
+          {products.map((product) => (
             <tr key={product._id}>
               <td>{product.title}</td>
               <td>
