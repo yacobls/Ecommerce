fdb7683bdc1c36801eb82a8da269778919b5da12
yacobls <yacob.lamrodo@gmail.com>
2024-12-17 15:16:05 +0700

    Add Cart function in Frontend

--- ecommerce-front/components/Button.js
+++ ecommerce-front/components/Button.js
@@ -16,6 +16,12 @@ export const ButtonStyle = css`
     margin-right: 5px;
   }
   ${(props) =>
+    props.block &&
+    css`
+      display: block;
+      width: 100%;
+    `}
+  ${(props) =>
     props.secondary &&
     !props.outline &&
     css`
@@ -29,6 +35,21 @@ export const ButtonStyle = css`
       background-color: transparent;
       color: #fff;
       border: 1px solid #fff;
+    `}
+    ${(props) =>
+    props.black &&
+    !props.outline &&
+    css`
+      background-color: #000;
+      color: #fff;
+    `}
+  ${(props) =>
+    props.black &&
+    props.outline &&
+    css`
+      background-color: transparent;
+      color: #000;
+      border: 1px solid #000;
     `}
   ${(props) =>
     props.primary &&
---
+++ ecommerce-front/models/Order.js
@@ -0,0 +1,14 @@
+import { model, models, Schema } from "mongoose";
+
+const OrderSchema = new Schema({
+  line_items: Object,
+  name: String,
+  email: String,
+  city: String,
+  postalCode: String,
+  streetAddress: String,
+  country: String,
+  paid: Boolean,
+});
+
+export const Order = models?.Order || model("Order", OrderSchema);
